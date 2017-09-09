dfa59f583eebc0742ce21166ce57d6f85ff15286
PaulRa <16649084+PaulRaUnite@users.noreply.github.com>
2017-09-09 10:47:15 +0300

    Search function 30% speed up, added benchmark and more documentation.

--- util/search.go
+++ util/search.go
@@ -1,43 +1,50 @@
 package util
 
 import (
-	"bufio"
 	"io"
 )
 
-// Search finds substring in reader, returns false if error happens
-// or it can't find the substring in reader.
-// Be sure, that your Reader is ReadSeeker
+// Search finds substring in reader, returns
+// false if error happens or it can't find
+// the substring in reader.
+//
+// Make sure, that your Reader is ReadSeeker
 // or you can start reading from it's origin
 // (you'll get nothing if you will try to
-// read after Search call)
+// read after Search call).
+//
+// Also, try to use buffers for readers of
+// files or net connections: Search reads
+// by single byte so bufferising can make
+// significant performance speed up.
 func Search(r io.Reader, text string) bool {
 	// It's nothing to do with empty text.
 	if len(text) == 0 {
 		return false
 	}
 
-	// Wrap reader with bufio.Reader to access ReadRune method.
-	br := bufio.NewReader(r)
+	// This construction is faster than make([]byte, 1, 1) or []byte{0}.
+	// The single byte is used to read from reader.
+	ba := [1]byte{0} //byte array
+	bs := ba[:]      //byte slice
+
 	// match keeps result of comparison in inner loop.
 	match := false
 	for {
-		// The inner loop does two things:
-		// - it gets new runes from reader,
-		//   and thus goes through all symbols
+		// This inner loop does two things:
+		// - it gets new byte from reader,
+		//   and thus goes through all bytes
 		//   in reader.
-		// - tries to find symbol where
-		//   substring starts and when
-		//   make sure that it really is
-		//   there.
-		for _, searchedSymbol := range text {
-			readerSymbol, _, err := br.ReadRune()
+		// - tries to find byte sequence
+		//   which starts as text
+		for i := 0; i < len(text); i++ {
+			_, err := r.Read(bs)
 			if err != nil {
 				return false
 			}
 
 			match = true
-			if searchedSymbol != readerSymbol {
+			if text[i] != bs[0] {
 				match = false
 				break
 			}
--- util/search_test.go
+++ util/search_test.go
@@ -32,3 +32,22 @@ func TestSearch(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkSearch(b *testing.B) {
+	const text = "こんいちは！私はポールです。わたしは１８せいです。よろしくね。ありがとございます。"
+	const searchText = "ありがと"
+
+	var rs io.ReadSeeker = strings.NewReader(text)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		found := Search(rs, searchText)
+		if found != true {
+			b.Fatal("search func doesn't works")
+		}
+		_, err := rs.Seek(0, io.SeekStart)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
